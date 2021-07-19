ab84ab8e441ab4a8e8eccd1bb67dd3a54c3c21cc
ghostbry1878 <flyx1878@outlook.com>
2021-07-19 14:35:10 -0500

    Signed-off-by: ghostbry1878 <flyx1878@outlook.com>

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+	echo "Calculadora";
+	function sumar($a, $b){
+		return $a + $b;	
+	}
+
+	function restar($a, $b){
+		return $a - $b;	
+	}
+
+	function multiplicar($a, $b){
+		return $a * $b;	
+	}
+	function dividir($a, $b){
+
+		if
+			return ("Error");
+		
+		return $a / $b;	
+	}
+	function potencia($a, $b){
+		return pow($a, $b);	
+	}
+	function resto($a, $b){
+
+		if ($b == 0)
+			return ("Error");
+		else 
+			return $a % $b;	
+
+	}
+
+?>
